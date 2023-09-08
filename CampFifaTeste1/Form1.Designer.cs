e52d969399630a92453dc355556919585de633c7
pacebr <vgdsno@gmail.com>
2023-09-08 16:31:46 -0300

    Adicionando o arquivo readme.md

---
+++ CampFifaTeste1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace CampFifaTeste1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.cb_nj = new System.Windows.Forms.ComboBox();
+            this.cb_nj2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(136, 210);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Enter += new System.EventHandler(this.comboBox1_Enter);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(522, 210);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.Enter += new System.EventHandler(this.comboBox2_Enter);
+            // 
+            // cb_nj
+            // 
+            this.cb_nj.FormattingEnabled = true;
+            this.cb_nj.Location = new System.Drawing.Point(263, 82);
+            this.cb_nj.Name = "cb_nj";
+            this.cb_nj.Size = new System.Drawing.Size(121, 21);
+            this.cb_nj.TabIndex = 0;
+            this.cb_nj.Enter += new System.EventHandler(this.cb_nj_Enter);
+            // 
+            // cb_nj2
+            // 
+            this.cb_nj2.FormattingEnabled = true;
+            this.cb_nj2.Location = new System.Drawing.Point(395, 82);
+            this.cb_nj2.Name = "cb_nj2";
+            this.cb_nj2.Size = new System.Drawing.Size(121, 21);
+            this.cb_nj2.TabIndex = 2;
+            this.cb_nj2.Enter += new System.EventHandler(this.cb_nj2_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cb_nj2);
+            this.Controls.Add(this.cb_nj);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox cb_nj;
+        private System.Windows.Forms.ComboBox cb_nj2;
+    }
+}
+
---
+++ CampFifaTeste1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using CampFifa;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CampFifaTeste1
+{
+    public partial class Form1 : Form
+    {
+        private bool comboBoxTimesFilled = false;
+        private bool comboBoxJogadoresFilled = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(@"Data Source=localhost\SQLEXPRESS; integrated security=SSPI;initial catalog=campeonato");
+        SqlCommand cm = new SqlCommand();
+        SqlDataReader dt;
+
+        private void LoadComboBoxTimes()
+        {
+            if (!comboBoxTimesFilled)
+            {
+                string vquerytimes = @"
+                SELECT
+                    id_times,
+                    nome_times
+                FROM
+                    times
+                ORDER BY 
+                    id_times
+                ";
+                var timesData = Banco.dql(vquerytimes); // Obtenha os dados uma vez
+
+                comboBox1.DataSource = timesData;
+                comboBox1.DisplayMember = "nome_times";
+                comboBox1.ValueMember = "id_times";
+
+                comboBox2.DataSource = timesData; // Preencha a outra ComboBox com os mesmos dados
+                comboBox2.DisplayMember = "nome_times";
+                comboBox2.ValueMember = "id_times";
+
+                comboBoxTimesFilled = true;
+            }
+        }
+
+        private void LoadComboBoxJogadores()
+        {
+            if (!comboBoxJogadoresFilled)
+            {
+                string vqueryjogadores = @"
+                SELECT
+                    id_jogador,
+                    nome_jogador
+                FROM
+                    jogadores
+                ORDER BY
+                    id_jogador
+                ";
+                var jogadoresData = Banco.dql(vqueryjogadores); // Obtenha os dados uma vez
+
+                cb_nj.DataSource = jogadoresData;
+                cb_nj.DisplayMember = "nome_jogador";
+                cb_nj.ValueMember = "id_jogador";
+
+                cb_nj2.DataSource = jogadoresData; // Preencha a outra ComboBox com os mesmos dados
+                cb_nj2.DisplayMember = "nome_jogador";
+                cb_nj2.ValueMember = "id_jogador";
+
+                comboBoxJogadoresFilled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Não preencha as ComboBoxes aqui; faça isso quando o usuário entrar nelas.
+        }
+
+        private void comboBox1_Enter(object sender, EventArgs e)
+        {
+            LoadComboBoxTimes();
+        }
+
+        private void comboBox2_Enter(object sender, EventArgs e)
+        {
+            LoadComboBoxTimes();
+        }
+
+        private void cb_nj_Enter(object sender, EventArgs e)
+        {
+            LoadComboBoxJogadores();
+        }
+
+        private void cb_nj2_Enter(object sender, EventArgs e)
+        {
+            LoadComboBoxJogadores();
+        }
+    }
+}
